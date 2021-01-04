b08e7820910d4449c38f4a0e02aee919933d404d
Guilherme Farias <guicfarias11@gmail.com>
2021-01-04 14:02:48 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,57 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer logo" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/42156470?s=460&u=00c46c0af7fa68c113730d973fdcdd3220d0db9e&v=4"
+            alt="Foto do dono do repositório"
+          />
+          <div>
+            <strong>guilherme-farias/github-explorer</strong>
+            <p>Aplicação em ReactJS utilizando a API do Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/42156470?s=460&u=00c46c0af7fa68c113730d973fdcdd3220d0db9e&v=4"
+            alt="Foto do dono do repositório"
+          />
+          <div>
+            <strong>guilherme-farias/github-explorer</strong>
+            <p>Aplicação em ReactJS utilizando a API do Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/42156470?s=460&u=00c46c0af7fa68c113730d973fdcdd3220d0db9e&v=4"
+            alt="Foto do dono do repositório"
+          />
+          <div>
+            <strong>guilherme-farias/github-explorer</strong>
+            <p>Aplicação em ReactJS utilizando a API do Github</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,94 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Title = styled.h1`
-  font-size: 48px;
   color: #3a3a4a;
+  font-size: 48px;
+  font-weight: bold;
+  margin-top: 80px;
+  max-width: 450px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  margin-top: 40px;
+  max-width: 700px;
+
+  input {
+    border: 0;
+    border-radius: 5px 0px 0px 5px;
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    color: #3a3a3a;
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    background: #04d361;
+    border: 0;
+    border-radius: 0px 5px 5px 0px;
+    color: #ffffff;
+    font-weight: bold;
+    height: 70px;
+    width: 210px;
+    transition: background-color 0.2s;
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  max-width: 700px;
+  margin-top: 80px;
+
+  a {
+    background: #ffffff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration: none;
+    display: block;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translate(10px);
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
 `;
